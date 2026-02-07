1a33b67462bc0e5f31e32ecdd151261ec5e0d9f8
MukulDhiman03 <dhimanmukul307@gmail.com>
2026-02-07 14:25:02 +0530

    feat: add expand/collapse

--- src/components/TreeNode.jsx
+++ src/components/TreeNode.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TreeNode = ({ node }) => {
+    const [isExpaned, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setIsExpanded((expand) => !expand);
+    }
     return (
         <div className='ml-[20px]'>
-            <div>{node.label}</div>
-            {node.children?.length > 0 && node.children.map(child => (
+            <div onClick={toggleExpand} style={{ cursor: "pointer" }}>
+                {node.label}
+                {node.children?.length > 0 && (
+                    <span>{
+                        isExpaned ? "🔽" : "🔼"
+                    }</span>
+                )}
+            </div>
+            {isExpaned && node.children?.map(child => (
                 <TreeNode key={child.id} node={child} />
             ))}
         </div>
+
     )
 }
 
